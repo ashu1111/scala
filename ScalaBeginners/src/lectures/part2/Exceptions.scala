6b11a1647d933ac30e164711f786dc316cd69f50
ashu1111 <ashutosh.rajput@gmail.com>
2020-03-22 11:50:24 -0400

    Updated Exceptions File

--- ScalaBeginners/src/lectures/part2/Exceptions.scala
+++ ScalaBeginners/src/lectures/part2/Exceptions.scala
@@ -2,7 +2,21 @@ package lectures.part2
 
 object Exceptions extends App {
 
-  val value: String = throw new OutOfMemoryError()
-  println(value)
+  //val value: String = throw new OutOfMemoryError()
 
+  class OverflowException extends RuntimeException
+  class UnderflowException extends RuntimeException
+  object PocketCalculator {
+    def add (x:Int, y:Int) = {
+      val result = x + y
+      if (x>0 && y>0 && result<0) throw new OverflowException
+      else if (x<0 && y <0 && result>0) throw new UnderflowException
+      else result
+    }
+
+  }
+
+  println(PocketCalculator.add(Int.MaxValue,4))
 }
+
+
